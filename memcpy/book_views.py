f0d52ac25b38df692ecd49583c6980da8e2598b0
bravoclaire <ybian1@andrew.cmu.edu>
2017-03-29 10:05:33 -0400

    browse books and pick flashcard, the image and audio cannot be shown

--- memcpy/book_views.py
+++ memcpy/book_views.py
@@ -5,9 +5,26 @@ from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.contrib import messages
+from memcpy.models import *
+
 
 def list_all_books(request):
-    context = {}
+    all_books = Book.objects.all()
+    all_entries = Entry.objects.all()
+
+    entry_total = []
+    for i in range(0, len(all_books)):
+        entry_list = []
+        for j in range(0, len(all_entries)):
+            if all_books[i] == all_entries[j].book:
+                entry_list.append(all_entries[j])
+        entry_total.append(entry_list)
+    book_total = []
+    for b in all_books:
+        book_total.append(b)
+
+    list = zip(book_total, entry_total)
+    context = {"list": list}
     messages.debug(request, 'List all books')
     return render(request, 'memcpy/books.html', context)
 
